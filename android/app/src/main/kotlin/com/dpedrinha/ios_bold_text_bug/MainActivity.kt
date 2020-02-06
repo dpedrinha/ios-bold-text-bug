56fe92217762e59d64cfd9c31d3cd0bed8a042a9
dpedrinha <dpedrinha@gmail.com>
2020-02-06 00:03:34 -0300

    .

---
+++ android/app/src/main/kotlin/com/dpedrinha/ios_bold_text_bug/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dpedrinha.ios_bold_text_bug
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
